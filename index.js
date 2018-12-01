41e67bf4e6201126e2ef1f3d49f8f8df23ddf4c5
zhaeong <owen_zzz@hotmail.com>
2018-12-01 18:55:17 -0500

    added a http call to duckduckgo

--- index.js
+++ index.js
@@ -25,9 +25,49 @@ app.post('/sms', (req, res) => {
 });
 
 
-app.get('/', (req, res) => res.send('peepee in my poopoo' + req.body.Body));
+app.get('/', (req, res) => res.send('peepee in dfdmy poopoo' + req.body.Body));
 
 app.get('/tester', (req, res) => res.send('resu:' + req));
+
+
+app.get('/get/*', (req, res) => 
+{
+	var searchRequest = req.originalUrl.split("/");
+	var searchTerm = searchRequest[2];
+
+	console.log("==========================");
+	console.log("=======get request========");
+	console.log(searchTerm);
+	console.log("==========================");
+	
+	http.get('http://api.duckduckgo.com/?q=' + searchTerm + '&format=json', (resp) => {
+	  let data = '';
+
+	  // A chunk of data has been recieved.
+	  resp.on('data', (chunk) => {
+	    data += chunk;
+	  });
+
+	  // The whole response has been received. Print out the result.
+	  resp.on('end', () => {
+	    
+	    var jsonResult = JSON.parse(data);
+
+	    //The null, 2 makes it print to console in pretty way
+	    var jsonString = JSON.stringify(jsonResult, null, 2); 
+
+	    res.send('result:<br>' + jsonString);
+
+	    console.log(jsonString);
+	  });
+
+	}).on("error", (err) => {
+	  console.log("Error: " + err.message);
+	});
+	
+});
+
+
 
 http.createServer(app).listen(8080, () => {
   console.log('Express server listening on port 8080');
